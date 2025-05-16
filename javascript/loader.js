a96e1f7902b1f0288b39c13d31c1aa025f79100e
Korfov17 <paco.80589@gmail.com>
2025-05-16 17:44:35 +0200

    Add files via upload

---
+++ javascript/loader.js
@@ -0,0 +1,7 @@
+
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    const loader = document.getElementById("loader-wrapper");
+    if (loader) loader.style.display = "none";
+  }, 4500);
+});
